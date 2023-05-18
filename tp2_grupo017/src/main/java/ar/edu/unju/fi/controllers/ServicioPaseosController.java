2fd51893f01fdbae2f1568a095f6e4ef31b3a02e
sebas <sebas.654@gmail.com>
2023-05-17 22:25:36 -0300

    ABM de paseadores terminado

--- tp2_grupo017/src/main/java/ar/edu/unju/fi/controllers/ServicioPaseosController.java
+++ tp2_grupo017/src/main/java/ar/edu/unju/fi/controllers/ServicioPaseosController.java
@@ -3,6 +3,7 @@ package ar.edu.unju.fi.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +16,7 @@ import ar.edu.unju.fi.models.Paseador;
 public class ServicioPaseosController {
     private String titulo;
     private boolean nuevo;
+    private String textoBoton;
     private Paseador paseador;
     
     private Paseadores paseadores = new Paseadores();
@@ -41,11 +43,13 @@ public class ServicioPaseosController {
         ModelAndView modelAndView = new ModelAndView("nuevo_paseador");
         this.titulo = "Nuevo Paseador";
         this.nuevo = true;
+        this.textoBoton = "Guardar";
         this.paseador = new Paseador();
         modelAndView.addObject("titulo", this.titulo);
         modelAndView.addObject("tituloFormulario", tituloForm);
         modelAndView.addObject("paseador", this.paseador);
         modelAndView.addObject("nuevo", this.nuevo);
+        modelAndView.addObject("textoBoton", this.textoBoton);
         return modelAndView;
     }
 
@@ -64,5 +68,35 @@ public class ServicioPaseosController {
         return "redirect:/paseos/paseadores";
     }
 
-    
+    @GetMapping("/paseadores/editar/{id}")
+    public ModelAndView getFormularioEmpleadoPage(@PathVariable(value = "id")int idPaseador){
+        ModelAndView modelAndView = new ModelAndView("nuevo_paseador");
+        String tituloForm = "Editar Datos Paseador";
+        this.titulo = "Editar Paseador";
+        this.nuevo = false;
+        this.textoBoton = "Actualizar";
+        Paseador paseadorEncontrado = this.paseadores.getPaseador(idPaseador);
+        
+        modelAndView.addObject("titulo", this.titulo);
+        modelAndView.addObject("tituloFormulario", tituloForm);
+        modelAndView.addObject("paseador", paseadorEncontrado);
+        modelAndView.addObject("nuevo", this.nuevo);
+        modelAndView.addObject("textoBoton", this.textoBoton);
+        return modelAndView;
+        
+    }
+
+    @PostMapping("/paseadores/editar")
+    public String modificarPaseador(@ModelAttribute(name = "paseador")Paseador paseador){
+        this.paseadores.modificarPaseador(paseador);
+        return "redirect:/paseos/paseadores";
+    }
+
+    @GetMapping("paseadores/eliminar/{id}")
+    public String eliminarPaseador(@ModelAttribute(value = "id")int idPaseador){
+        this.paseadores.eliminarPaseador(idPaseador);
+        return "redirect:/paseos/paseadores";
+    }
+
+
 }
