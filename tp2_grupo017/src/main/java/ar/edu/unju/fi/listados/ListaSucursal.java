37e40a3cd6eb6c8f7acceb5f6738df96dcacaab8
SOSA <SOSA@DESKTOP-V4PQRV0.mshome.net>
2023-06-04 18:06:31 -0300

    Aplicando Bootstrap y dependencias en Sucursales

--- tp2_grupo017/src/main/java/ar/edu/unju/fi/listados/ListaSucursal.java
+++ tp2_grupo017/src/main/java/ar/edu/unju/fi/listados/ListaSucursal.java
@@ -1,18 +1,24 @@
 package ar.edu.unju.fi.listados;
  
 import java.util.List;
+
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 import ar.edu.unju.fi.models.*;
 
+@Component
 public class ListaSucursal {
 	private List<Sucursal> sucursales;
 
 	
 	public ListaSucursal() {
 		sucursales = new ArrayList<Sucursal>();
-		sucursales.add(new Sucursal("amigos", "Alvear", "Jujuy", LocalDate.of(2020, 2, 12), "facu_uama", "0388"));
+		sucursales.add(new Sucursal("Pata de Oro - Centro", "Alvear 1609", "S.S. de Jujuy", LocalDate.of(2020, 2, 12), "gold_alvear@gmail.com", "0388155555555"));
+		sucursales.add(new Sucursal("Pata de Oro - Perico", "Chaile 99", "Perico", LocalDate.of(2020, 6, 21), "gold_chaile@gmail.com", "0388151111111"));
+		sucursales.add(new Sucursal("Pata de Oro - Libertador", "Cofre 543", "Libertador", LocalDate.of(2021, 1, 31), "gold_lib@gmail.com", "0388152222222"));
 	}
 	
 	public List<Sucursal> getSucursales(){
