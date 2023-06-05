aa6bf5984c8ed1a547a19917937a170d6addcfc4
Joel <Joel@DESKTOP-IFLVLL6>
2023-06-04 22:55:50 -0300

    Validacion de contactenos con spring-boot-starter-validation

--- tp2_grupo017/src/main/java/ar/edu/unju/fi/controllers/ContactenosController.java
+++ tp2_grupo017/src/main/java/ar/edu/unju/fi/controllers/ContactenosController.java
@@ -1,20 +1,34 @@
 package ar.edu.unju.fi.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
-@RequestMapping("/contactenos")
-public class ContactenosController {
-    private String titulo;
+import ar.edu.unju.fi.models.ContactenosModel;
+import jakarta.validation.Valid;
 
-    @GetMapping("")
-    public ModelAndView getContactenosPage(){
-        ModelAndView modelAndView = new ModelAndView("contactenos");
-        this.titulo = "contactenos";
-        modelAndView.addObject("titulo", this.titulo);
-        return modelAndView; 
-    }
+    @Controller
+    public class ContactenosController {    
+
+    @GetMapping("/contactenos")
+	public String getContactenos(Model model) {
+		    model.addAttribute("contactenosModel", new ContactenosModel());
+		    return "contactenos";
+	}
+	
+	
+	@PostMapping("/enviar")
+	public String enviarFormulario(@ModelAttribute("contactenosModel") @Valid ContactenosModel contactenosModel,
+            BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+		return "contactenos";
+		}
+
+			// Procesar el formulario y enviar el mensaje
+
+		return "redirect:/contactenos";
+}
 }
