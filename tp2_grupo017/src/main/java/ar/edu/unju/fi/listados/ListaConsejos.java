4afb6cf6150b41a6db5e6a2a4a5f2f6cdb11e2e4
Joel <Joel@DESKTOP-IFLVLL6>
2023-06-11 08:11:54 -0300

    Se ha implementado la capa service con su respectiva documentacion.

--- tp2_grupo017/src/main/java/ar/edu/unju/fi/listados/ListaConsejos.java
+++ tp2_grupo017/src/main/java/ar/edu/unju/fi/listados/ListaConsejos.java
@@ -3,8 +3,14 @@ package ar.edu.unju.fi.listados;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import ar.edu.unju.fi.models.Consejos;
 
+/**
+ * Componente que almacena y gestiona una lista de consejos.
+ */
+@Component
 public class ListaConsejos {
 
 	private List<Consejos> consejos;
@@ -17,16 +23,29 @@ public class ListaConsejos {
         return consejos;
     }
 
+    /**
+     * Agrega un consejo a la lista.
+     * @param consejo El consejo a agregar
+     */
     public void agregarConsejo(Consejos consejo) {
         consejos.add(consejo);
     }
 
+    /**
+     * Edita un consejo existente en la lista.
+     * @param indice El índice del consejo a editar
+     * @param nuevoConsejo El nuevo consejo que reemplazará al existente
+     */
     public void editarConsejo(int indice, Consejos nuevoConsejo) {
         if (indice >= 0 && indice < consejos.size()) {
             consejos.set(indice, nuevoConsejo);
         }
     }
 
+    /**
+     * Elimina un consejo de la lista.
+     * @param indice El índice del consejo a eliminar
+     */
     public void eliminarConsejo(int indice) {
         if (indice >= 0 && indice < consejos.size()) {
             consejos.remove(indice);
--- tp2_grupo017/src/main/java/ar/edu/unju/fi/services/IConsejoService.java
+++ tp2_grupo017/src/main/java/ar/edu/unju/fi/services/IConsejoService.java
@@ -3,6 +3,10 @@ package ar.edu.unju.fi.services;
 import java.util.List;
 
 import ar.edu.unju.fi.models.Consejos;
+/**
+ * Esta interfaz proporciona los métodos para administrar los consejos.
+ * Permite listar, guardar, modificar y eliminar consejos.
+ */
 
 public interface IConsejoService {
 
