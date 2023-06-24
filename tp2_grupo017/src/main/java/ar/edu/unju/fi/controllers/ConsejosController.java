c0e97082dc51befc31495dcc511c96e3fbec318b
Joel <Joel@DESKTOP-IFLVLL6>
2023-06-24 15:25:59 -0300

    Los consejos de ejemplo ahora se inicializan en ListaConsejos.java

--- tp2_grupo017/src/main/java/ar/edu/unju/fi/controllers/ConsejosController.java
+++ tp2_grupo017/src/main/java/ar/edu/unju/fi/controllers/ConsejosController.java
@@ -29,14 +29,14 @@ public class ConsejosController {
 	 */
     public ConsejosController(IConsejoService consejoService) {
     	this.consejoService = consejoService;
-    	//Se agregan consejos de ejemplo.
+    	/* se movieron a ListaConsejos.java
     	consejoService.guardarConsejo(new Consejos("Mantén a tu mascota al día con sus vacunas: Es importante que tu mascota esté al día con sus vacunas para protegerla contra enfermedades peligrosas. Consulta con tu veterinario para determinar qué vacunas son necesarias para tu mascota."));
     	consejoService.guardarConsejo(new Consejos("Dale una dieta equilibrada: Una dieta equilibrada es esencial para la salud de tu mascota. Asegúrate de proporcionarle una alimentación adecuada y evitar darle alimentos que no sean adecuados para su especie."));
     	consejoService.guardarConsejo(new Consejos("Evita productos tóxicos: Muchos productos del hogar, como plantas y productos químicos, pueden ser tóxicos para las mascotas. Mantén estos productos fuera del alcance de tu mascota para evitar cualquier peligro."));
     	consejoService.guardarConsejo(new Consejos("Monitorea su salud dental: La salud dental es importante para la salud general de tu mascota. Asegúrate de cepillarle los dientes regularmente y proporcionarle juguetes adecuados para ayudar a mantener sus dientes limpios y sanos."));
     	consejoService.guardarConsejo(new Consejos("Presta atención a su comportamiento: Tu mascota no puede decirte cuándo algo no está bien, por lo que es importante prestar atención a su comportamiento y detectar cualquier cambio que pueda indicar un problema de salud. Si notas algún cambio en el comportamiento de tu mascota, consulta a tu veterinario."));
     	consejoService.guardarConsejo(new Consejos("Proporciona un ambiente seguro: Mantén a tu mascota en un ambiente seguro y protegido, lejos de peligros potenciales como calles transitadas, cuerpos de agua y productos químicos peligrosos."));
-        
+        */
     }
     
     /**
