3940c99b9e1f885cfbf03fa1516f56b23f5de03a
Sebastian Farfan <sebas.654@gmail.com>
2023-07-06 03:35:35 -0300

    agregó validaciones a provincias

--- tp2_grupo017/src/main/java/ar/edu/unju/fi/controllers/ProvinciaController.java
+++ tp2_grupo017/src/main/java/ar/edu/unju/fi/controllers/ProvinciaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import ar.edu.unju.fi.entity.Provincia;
 import ar.edu.unju.fi.services.IProvinciaService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/provincia")
 public class ProvinciaController {
-    
+
     @Autowired
     @Qualifier("provinciaServiceMysql")
     private IProvinciaService provinciaService;
@@ -29,16 +31,24 @@ public class ProvinciaController {
         model.addAttribute("provincias", this.provinciaService.getByEstado(true));
         return "gestion_provincias";
     }
-    
+
     @PostMapping("/guardar")
-    public String guardarProvincia(@ModelAttribute("provincia")Provincia provincia){
+    public String guardarProvincia(@Valid @ModelAttribute("provincia") Provincia provincia, BindingResult result,
+            Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", "Provincias");
+            model.addAttribute("tituloForm", "Registro Provincia");
+            model.addAttribute("provincia", provincia);
+            model.addAttribute("provincias", this.provinciaService.getByEstado(true));
+            return "gestion_provincias";
+        }
         provincia.setEstado(true);
         this.provinciaService.guardarProvincia(provincia);
         return "redirect:/provincia/gestion";
     }
 
     @GetMapping("/editar/{codigo}")
-    public String getEditarPage(@PathVariable(value = "codigo")Long codigo, Model model) {
+    public String getEditarPage(@PathVariable(value = "codigo") Long codigo, Model model) {
         Provincia provinciaBuscada = this.provinciaService.getByCodigo(codigo);
         model.addAttribute("titulo", "Provincias");
         model.addAttribute("tituloForm", "Registro Provincia");
@@ -48,9 +58,9 @@ public class ProvinciaController {
     }
 
     @GetMapping("/eliminar/{codigo}")
-    public String getEliminar(@PathVariable(value = "codigo")Long codigo) {
+    public String getEliminar(@PathVariable(value = "codigo") Long codigo) {
         this.provinciaService.eliminarProvinciaByCodigo(codigo);
-        
+
         return "redirect:/provincia/gestion";
     }
 }
